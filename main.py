7de91609a83d996e9d264e8baee31457ab308f9f
glauberrleite <grl@ic.ufal.br>
2017-10-23 18:51:48 -0300

    Testing spectrum

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import scipy.io.wavfile
 
-samplerate, data = scipy.io.wavfile.read("sample.wav");
+samplerate, data = scipy.io.wavfile.read("sample.wav")
+
+N = 4*samplerate
+x = scipy.fft(x[:N])
+
+gain = 20*scipy.log10(scipy.absolute(x))
+
+f = scipy.linspace(0, samplerate, N, endpoint=False)
+
+pylab.plot(f, gain)
+
+pylab.xlim(0, 5000)
