f4cc5a0051d73a0bcc00aa2bb232732b237f70b2
Luís Felipe <felipemmha@gmail.com>
2017-10-26 21:16:23 -0300

    Errors corrections
    Signed-off-by: Luís Felipe <felipemmha@gmail.com>

--- plotSpectrum.py
+++ plotSpectrum.py
@@ -2,23 +2,24 @@ import scipy.io.wavfile
 import matplotlib.pyplot as pyplot
 
 
-class Spectrum(sample):
-    def __init__(self):
-        self.samplerate, self.data = scipy.io.wavefile.read(sample)
+class Spectrum():
+    def __init__(self, sample):
+        self.samplerate, self.data = scipy.io.wavfile.read(sample)
 
     def plot(self):
         N = 4 * self.samplerate
         x = scipy.fft(self.data[:N])
         gain = 20 * scipy.log10(scipy.absolute(x))
-        f = scipy.linspace(0, samplerate, N, endpoint=False)
+        f = scipy.linspace(0, self.samplerate, N, endpoint=False)
         pyplot.plot(f, gain)
         pyplot.xlim(0, 5000)
         pyplot.show()
 
 
 def main():
+    print("Drawing sample...")
     sample = Spectrum("sample.wav")
     sample.plot()
 
-if __name__ == '_main__':
+if __name__ == '__main__':
     main()
