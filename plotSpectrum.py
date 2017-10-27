55336ed098d3204c745390d4e99042720bedddea
glauberrleite <grl@ic.ufal.br>
2017-10-26 23:26:57 -0300

    Animation working and bash script added

--- plotSpectrum.py
+++ plotSpectrum.py
@@ -1,17 +1,36 @@
 import scipy.io.wavfile
 import matplotlib.pyplot as pyplot
+import matplotlib.animation
 
+# Reading audio file and collecting its relevant info
 samplerate, data = scipy.io.wavfile.read("sample.wav")
+duration = len(data)/samplerate
 
-N = 4*samplerate
-x = scipy.fft(data[:N])
+# Setting STEP for the number of seconds for delay
+# and n for the corresponding number of frames in data array
+STEP = 1
+N = STEP * samplerate
 
-gain = 20*scipy.log10(scipy.absolute(x))
+# Preparing graph
+fig = pyplot.figure()
+ax = pyplot.axes(xlim=(0, 5000), ylim=(75, 175))
+line, = ax.plot([], [])
 
-f = scipy.linspace(0, samplerate, N, endpoint=False)
+def animate(i):
+    start = int(round(i * samplerate))
+    end = int(round((i + STEP) * samplerate))
 
-pyplot.plot(f, gain)
+    fourier = scipy.fft(data[start:end])
 
-pyplot.xlim(0, 5000)
+    gain = 20*scipy.log10(scipy.absolute(fourier))
+
+    freq = scipy.linspace(0, samplerate, STEP*samplerate, endpoint=False)
+
+    line.set_data(freq, gain)
+
+    return line,
+
+
+anim = matplotlib.animation.FuncAnimation(fig, animate, frames=int(round(duration/STEP)), interval=(STEP * 1000), blit=True)
 
 pyplot.show()
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python playSample.py &
+python plotSpectrum.py
